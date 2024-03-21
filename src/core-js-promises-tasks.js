f7f00fa3f1ffccb022e8394f8318d42f39b4fef1
ilonaand <ilonaand@gmail.com>
2024-03-21 17:44:19 +0300

    fix: 6 from 7

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -110,8 +110,13 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  Promise.allSettled(promises).then((values) => {
+    const p = values.map((item) =>
+      item.status === 'fulfilled' ? item.value : null
+    );
+    return Promise.resolve(p);
+  });
 }
 
 /**
@@ -132,8 +137,10 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reduce((arr, cur) => {
+    return cur.then((value) => arr.then((a) => a + value));
+  }, Promise.resolve(''));
 }
 
 module.exports = {
